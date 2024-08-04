c11d0db8304c048f0804188f103381eaf0682104
Priyanshu Choudhary <133704411+pc-hover@users.noreply.github.com>
2024-08-04 23:18:52 +0530

    Cutsom hook - Online status

--- CustomHooks/CustomHook/src/App.jsx
+++ CustomHooks/CustomHook/src/App.jsx
@@ -36,12 +36,35 @@ function useTodos(n) {
     loading: loading
   };
 }
+//custom  hook for checking online status
+function useIsOnline() {
+
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+  useEffect(() => {
+    //this logic is for when we will change online status
+    window.addEventListener("online", () => {
+      setIsOnline(true);
+    })
+    window.addEventListener("offline", () => {
+      setIsOnline(false);
+    })
+    //In case isOnline chnages in dependency this will run
+
+  }, [])
+
+  return isOnline;
+}
 
 function App() {
 
-  const { todos, loading } = useTodos(2);
-
-
+  const { todos, loading } = useTodos(5);
+  const isOnline = useIsOnline();
+  if (isOnline) {
+    return "You are online"
+  }
+  else {
+    return "You are offline"
+  }
   if (loading) {
     return <div>
       Loading...
@@ -51,6 +74,7 @@ function App() {
   return (
     <>
       {todos.map(todo => <Track todo={todo} />)}
+
     </>
   )
 }
